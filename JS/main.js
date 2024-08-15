6b3216b69567f85dd477c038f4fc1d2072918f75
MD. TARIKUL ISLAM <sifatislam1316@gmail.com>
2024-08-15 20:31:58 +0600

    Copy right mark

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,16 @@
 const collapseFlyMenu = () => {
   document.body.classList.toggle("fly-menu");
 };
+(function ($) {
+  $(document).ready(function ($) {
+    $(".pagination-action .preview").click(function (e) {
+      const contentWidth = $(".slide-item .text-content").width();
+      $(".pagination-slider .wrapper").scrollLeft -= contentWidth;
+    });
 
-const paginationSlider = document.querySelector(".pagination-slider .wrapper");
-const prevBtn = document.querySelector(".pagination-action .preview");
-const nextBtn = document.querySelector(".pagination-action .next");
-nextBtn.addEventListener("click", () => {
-  const contentWidth = paginationSlider.querySelector(
-    ".slide-item .text-content"
-  ).clientWidth;
-  paginationSlider.scrollLeft += contentWidth;
-});
-prevBtn.addEventListener("click", () => {
-  const contentWidth = paginationSlider.querySelector(
-    ".slide-item .text-content"
-  ).clientWidth;
-  paginationSlider.scrollLeft -= contentWidth;
-});
+    $(".pagination-action .next").click(function (e) {
+      const contentWidth = $(".slide-item .text-content").width();
+      $(".pagination-slider .wrapper").scrollLeft += contentWidth;
+    });
+  });
+})(jQuery);
